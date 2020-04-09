3b159cc5871ff26e4928e0f73be39fdbb17f3b18
Hako <efim@ashkinazi.co.il>
2020-04-09 21:56:06 +0300

    Adding ServiceAPI Implementation

--- GuiPsExec/WinFramework/ServiceApi/ServiceApi.cpp
+++ GuiPsExec/WinFramework/ServiceApi/ServiceApi.cpp
@@ -12,3 +12,71 @@ SC_HANDLE WinFramework::ServiceApi::OpenSCMManagerW(std::wstring& machineName, s
 
 	return handle;
 }
+
+SC_HANDLE WinFramework::ServiceApi::CreateServiceW(SC_HANDLE scmHandle, std::wstring& serviceName, std::wstring& displayName, DWORD desiredAccess, DWORD serviceType, DWORD startType, DWORD errorControl, std::wstring& binaryPathName, std::wstring& loadOrderGroup, LPDWORD tagId, std::wstring& dependencies, std::wstring& serviceStartName, std::wstring& password)
+{
+	SC_HANDLE handle = ::CreateServiceW(scmHandle,
+		serviceName.c_str(),
+		displayName.c_str(),
+		desiredAccess,
+		serviceType,
+		startType,
+		errorControl,
+		binaryPathName.c_str(),
+		loadOrderGroup.c_str(),
+		tagId,
+		dependencies.c_str(),
+		serviceStartName.c_str(),
+		password.c_str());
+
+	if (NULL == handle)
+	{
+		throw WinFramework::GeneralWin32Exceptions::UnkownWin32Exception();
+	}
+
+	return handle;
+}
+
+VOID WinFramework::ServiceApi::StartServiceW(SC_HANDLE serviceHandle, std::vector<std::wstring> serviceArgs)
+{
+	PWCHAR * serviceArgsStr = nullptr;
+	if (0 != serviceArgs.size())
+	{
+		std::size_t totalSize = GetStringsTotalSizeInVector(serviceArgs);
+		serviceArgsStr = static_cast<PWCHAR *>(::HeapAlloc(::GetProcessHeap(), HEAP_ZERO_MEMORY | HEAP_GENERATE_EXCEPTIONS, totalSize));
+		CopyStringsFromVectorToMemoryArray(serviceArgs, serviceArgsStr);
+	}
+
+	BOOL res = ::StartServiceW(serviceHandle,
+		serviceArgs.size(),
+		serviceArgsStr);
+
+	if (FALSE == res)
+	{
+		throw WinFramework::GeneralWin32Exceptions::UnkownWin32Exception();
+	}
+}
+
+
+std::size_t GetStringsTotalSizeInVector(std::vector<std::wstring>& strVector)
+{
+	std::size_t totalSize = 0;
+	for (std::wstring str : strVector)
+	{
+		totalSize += str.size();
+	}
+
+	return totalSize;
+}
+
+
+VOID CopyStringsFromVectorToMemoryArray(std::vector<std::wstring> strVector, PWCHAR * strArray)
+{
+	UINT32 i = 0;
+	for (std::wstring str : strVector)
+	{
+		strArray[i] = static_cast<PWCHAR>(::HeapAlloc(::GetProcessHeap(), HEAP_ZERO_MEMORY | HEAP_GENERATE_EXCEPTIONS, str.size()));
+		::CopyMemory(strArray[i], str.data(), str.size());
+		i++;
+	}
+}
--- GuiPsExec/WinFramework/ServiceApi/ServiceApi.hpp
+++ GuiPsExec/WinFramework/ServiceApi/ServiceApi.hpp
@@ -2,6 +2,7 @@
 
 #include <Windows.h>
 #include <string>
+#include <vector>
 
 // TODO :: Write UniqueResource
 
@@ -9,10 +10,12 @@ namespace WinFramework
 {
 namespace ServiceApi
 {
+	// YA :: TODO :: Add Tests
 	// YA :: TODO :: Change to autohandle
 	SC_HANDLE OpenSCMManagerW(std::wstring& machineName, std::wstring& database, DWORD desiredAccess);
 
-	// CreateServiceA 
-	// StartServices 
+	SC_HANDLE CreateServiceW(SC_HANDLE scmHandle, std::wstring& serviceName, std::wstring& displayName, DWORD desiredAccess, DWORD serviceType, DWORD startType, DWORD errorControl, std::wstring& binaryPathName, std::wstring& loadOrderGroup, LPDWORD tagId, std::wstring& dependencies, std::wstring& serviceStartName, std::wstring& password);
+	
+	VOID StartServiceW(SC_HANDLE serviceHandle, std::vector<std::wstring> serviceArgs);
 }
 }
